f0a41d72f89794425071f68497ed8719a0a6d6bb
Jack Keepin <jack.keepin@students.plymouth.ac.uk>
2020-12-30 15:01:57 +0000

    Updated test

--- test/unit.js
+++ test/unit.js
@@ -6,7 +6,9 @@ suite("Test test", function() {
       // Initialise a counter.
       let counter = 0;
   
-      for (let i=0; i<100; i++) {}
+      for (let i=0; i<100; i++) {
+          counter++;
+      }
   
       chai.assert.equal(counter, 100, "Answer should be 100");
     });
