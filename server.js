f58a4d3537b4fe639d8869a51e03ab9f042c8e15
Jack Keepin <jack.keepin@students.plymouth.ac.uk>
2020-12-30 15:35:02 +0000

    updated server code

--- server.js
+++ server.js
@@ -3,14 +3,16 @@ let http = require("http");
 let path = require("path");
 let socketio = require("socket.io");
 let mongoose = require("mongoose");
+const port = process.env.PORT || 9000;
+
 
 let app = express();
 let server = http.createServer(app);
 
 app.use(express.static(path.join(__dirname, "resources")));
 
-server.listen(9000, function() {
-    console.log("Server listening on port 9000");
+server.listen(port, function() {
+    console.log("Server listening on port " + port);
 });
 
 let io = socketio(server);
