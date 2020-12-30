363f9d0845c729d246156d3acea0f700046fcdfc
Jack Keepin <jack.keepin@students.plymouth.ac.uk>
2020-12-30 14:59:21 +0000

    Updated package and travis. Also added quick unit test

---
+++ test/unit.js
@@ -0,0 +1,14 @@
+let chai = require("chai");
+
+suite("Test test", function() {
+
+    test("Quick test", function() {
+      // Initialise a counter.
+      let counter = 0;
+  
+      for (let i=0; i<100; i++) {}
+  
+      chai.assert.equal(counter, 100, "Answer should be 100");
+    });
+  
+  });
